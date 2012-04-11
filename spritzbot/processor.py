658f433cdd08ef55c575c33c257821cda163cf81
Harshad Sharma <harshad@harshadsharma.com>
2012-04-12 03:42:23 +0530

    Moving to a class based style for processor.py

--- spritzbot/processor.py
+++ spritzbot/processor.py
@@ -2,31 +2,30 @@ import os
 import re
 import imp
 
-PLUGINS = {}
-COMMANDS = {}
-BASE_PATH = os.path.dirname(os.path.realpath(__file__))
-PLUGIN_PATH = os.path.join(BASE_PATH, "plugins")
+class TweetProcessor:
+    plugins = {}
+    commands = []
+    base_path = os.path.dirname(os.path.realpath(__file__))
+    plugin_path = os.path.join(base_path, "plugins")
+    
+    def load_plugins(self):
+        """Loads plugins and associated commands."""
+        # Filename pattern that we want to load.
+        re_plugin = re.compile('[^.].*\.py$')
+        
+        for plugin_module in os.listdir(self.plugin_path):
+            if re_plugin.match(plugin_module):
+                # Get the module's name
+                name = plugin_module[:-3]
+                plugin_info = imp.find_module(name, [self.plugin_path])
+                plugin = imp.load_module(name, *plugin_info)
+                self.plugins.update({name:plugin})
+                
+                for command in plugin.commands():
+                    self.commands.append({name:command})
+    
 
-def load_plugins():
-    """Loads all plugins and the associated commands.
-    Code borrowed from gtalkbot.py
-    """
-    global PLUGINS, COMMANDS, PLUGIN_PATH
-    re_plugin = re.compile('[^.].*\.py$')
-    loaded_plugins = []
-    for plugin_file in os.listdir(PLUGIN_PATH):
-        if re_plugin.match(plugin_file):
-            name = plugin_file[:-3]
-            plugin_info = imp.find_module(name, [PLUGIN_PATH])
-            plugin = imp.load_module(name, *plugin_info)
-            PLUGINS[name] = plugin
-            loaded_plugins.append(name)
-            # Load the commands supported by this plugin
-            for command in plugin.commands():
-                COMMANDS.update({name:command})
-
-    return loaded_plugins
-
-print load_plugins()
-print COMMANDS
-print PLUGINS
+tp = TweetProcessor()
+tp.load_plugins()
+print tp.commands
+        
